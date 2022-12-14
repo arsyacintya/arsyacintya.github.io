07a449c613217a3591b851ccf33aa06e3bb99c67
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2022-12-14 18:51:38 +0700

    Update rexdir.js

--- rexdir.js
+++ rexdir.js
@@ -1,12 +1,13 @@
-$(document).ready(function()
-	{
-	  $('body').addClass('xepo_ads');
-	});
-
-	$(document).on('click','.xepo_ads',function(e)
-	{
-	    $(this).removeClass('xepo_ads');
-
-	    //ubah google dengan ads direct link
-	    window.open('https://www.highperformancecpmgate.com/arwgc4a3gj?key=32e55ecf41ec9483a4c30821e28dcfeb', '_blank');
-	});
+const aScript = document.createElement("script");
+aScript.setAttribute("src","https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js");
+document.body.append(aScript);
+aScript.onload=()=>{
+$(document).ready(function(){
+$('body').addClass('xepo_ads');
+});
+$(document).on('click','.xepo_ads',function(e){
+$(this).removeClass('xepo_ads');
+//ubah google dengan ads direct link
+window.open('https://www.highperformancecpmgate.com/arwgc4a3gj?key=32e55ecf41ec9483a4c30821e28dcfeb', '_blank');
+});
+};
