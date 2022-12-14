a05e80611000fcd248a5243e5bb966e3e89146e9
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2022-12-14 07:14:01 +0700

    Create rexdir.js

---
+++ rexdir.js
@@ -0,0 +1,12 @@
+$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    //ubah google dengan ads direct link
+	    window.open('https://www.highperformancecpmgate.com/arwgc4a3gj?key=32e55ecf41ec9483a4c30821e28dcfeb', '_blank');
+	});
