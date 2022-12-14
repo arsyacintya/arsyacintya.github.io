5dfa09c2459731574b04ba2763df208514b31f86
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2022-12-14 12:46:44 +0700

    Create socbar.js

---
+++ socbar.js
@@ -0,0 +1,1 @@
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://pl15808188.highperformancecpmgate.com/34/c7/52/34c752f0a744d8e421efa2cd13e5d186.js"></scr' + 'ipt>');
