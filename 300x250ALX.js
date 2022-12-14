2ab047f10ec1197db51507e6d2a667e17e723217
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2022-12-14 07:43:37 +0700

    Create 300x250ALX.js

---
+++ 300x250ALX.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '35ff9bfde9c6692e37b4dc39e859a703',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/35ff9bfde9c6692e37b4dc39e859a703/invoke.js"></scr' + 'ipt>');
