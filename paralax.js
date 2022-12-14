cb2d7be881fed5b86606b42520ca0d8304c2e63c
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2022-12-14 18:17:30 +0700

    Create paralax.js

---
+++ paralax.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '5cc25e64f4ac5746eba90136f59fb246',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/5cc25e64f4ac5746eba90136f59fb246/invoke.js"></scr' + 'ipt>');
