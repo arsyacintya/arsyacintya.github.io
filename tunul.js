cb63be3a8068892ce0e57f36b50bea434ca626ab
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2022-12-31 14:38:31 +0700

    Create tunul.js

---
+++ tunul.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '8da6deb0bfbda4ed848b5f82176a8e9b',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/8da6deb0bfbda4ed848b5f82176a8e9b/invoke.js"></scr' + 'ipt>');
