fed8b9860559b44a47923030179042023215c33a
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2023-05-05 16:02:50 +0700

    Delete native.js

--- chacreation/native.js
+++
@@ -1,2 +0,0 @@
-<script async="async" data-cfasync="false" src="//pl15808189.highrevenuegate.com/1bd1934a2982baedfa995b272d386d33/invoke.js"></script>
-<div id="container-1bd1934a2982baedfa995b272d386d33"></div>
