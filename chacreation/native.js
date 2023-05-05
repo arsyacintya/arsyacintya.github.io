0236dff9fb18a33fc15036eb881bd2a0b32e6432
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2023-05-05 16:05:04 +0700

    Create native.js

---
+++ chacreation/native.js
@@ -0,0 +1,2 @@
+document.write('<script async="async" data-cfasync="false" src="//pl15808189.highrevenuegate.com/1bd1934a2982baedfa995b272d386d33/invoke.js"></script>');
+document.write('<div id="container-1bd1934a2982baedfa995b272d386d33"></div>');
