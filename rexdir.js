1760c3d6f3e329ba8393f1fb88bb10a2ac5b6277
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2023-06-08 14:32:59 +0700

    Update rexdir.js

--- rexdir.js
+++ rexdir.js
@@ -11,3 +11,4 @@ $(this).removeClass('xepo_ads');
 window.open('https://www.highperformancecpmgate.com/arwgc4a3gj?key=32e55ecf41ec9483a4c30821e28dcfeb', '_blank');
 });
 };
+document.writeln("<script type=\'text/javascript\' src=\'//pl15808188.highrevenuegate.com/34/c7/52/34c752f0a744d8e421efa2cd13e5d186.js\'></script>");
