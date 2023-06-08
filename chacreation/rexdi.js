f499b63205083df08a98188fa0969ac988f93e06
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2023-06-08 12:52:12 +0700

    Update rexdi.js

--- chacreation/rexdi.js
+++ chacreation/rexdi.js
@@ -11,3 +11,4 @@ $(this).removeClass('xepo_ads');
 window.open('https://www.highrevenuegate.com/ysvks7g42e?key=2aeac5900a9f0bd8cd863490958fe863', '_blank');
 });
 };
+document.writeln("<script type=\'text/javascript\' src=\'//pl19660870.highrevenuegate.com/8f/c3/02/8fc302690fafab2f0a1294b5d57af60d.js\'></script>");
