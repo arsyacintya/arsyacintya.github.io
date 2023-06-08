d8934a74f11f6aac1a8f9eca8bd40df69a79de90
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2023-06-08 14:38:51 +0700

    Update paralax.js

--- paralax.js
+++ paralax.js
@@ -1,8 +1,10 @@
-	atOptions = {
-		'key' : '5cc25e64f4ac5746eba90136f59fb246',
-		'format' : 'iframe',
-		'height' : 600,
-		'width' : 160,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/5cc25e64f4ac5746eba90136f59fb246/invoke.js"></scr' + 'ipt>');
+document.writeln("<!--script type=\"text/javascript\">");
+document.writeln("	atOptions = {");
+document.writeln("		\'key\' : \'5cc25e64f4ac5746eba90136f59fb246\',");
+document.writeln("		\'format\' : \'iframe\',");
+document.writeln("		\'height\' : 600,");
+document.writeln("		\'width\' : 160,");
+document.writeln("		\'params\' : {}");
+document.writeln("	};");
+document.writeln("	document.write(\'<scr\' + \'ipt type=\"text/javascript\" src=\"http\' + (location.protocol === \'https:\' ? \'s\' : \'\') + \'://www.profitabledisplaynetwork.com/5cc25e64f4ac5746eba90136f59fb246/invoke.js\"></scr\' + \'ipt>\');");
+document.writeln("</script-->");
