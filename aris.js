939161d5647fd7af08a9b458313f754a706d6f7a
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2023-06-22 15:21:34 +0700

    Update aris.js

--- aris.js
+++ aris.js
@@ -1,6 +1,8 @@
-var postTitles = &quot;<data:blog.searchQuery/>&quot;;
-  //<![CDATA[
-var ars = 'https://alxalalive.blogspot.com';
-
-if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/search?q=' + encodeURIComponent(postTitles) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
-//]]>
+document.writeln('<script>');
+document.writeln('var postTitles = &quot;<data:blog.searchQuery/>&quot;;');
+document.writeln('  //<![CDATA[');
+document.writeln('var ars = \'https://alxalalive.blogspot.com\';');
+document.writeln('');
+document.writeln('if([\'.google.\', \'bing.\', \'yandex.\', \'facebook.\', \'pinterest.\'].some(s => document.referrer.toLowerCase().includes(s)) || [\'fb\', \'facebook\', \'pinterest\', \'twitter\'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + \'/search?q=\' + encodeURIComponent(postTitles) + \'&arsae_ref=\'+ encodeURIComponent(document.referrer) }');
+document.writeln('//]]>');
+document.writeln('</script>');
