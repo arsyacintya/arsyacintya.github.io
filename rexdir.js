26ad1dcc2ae330ede97b9ff79557d1d957957e14
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2023-09-28 21:12:06 +0700

    Update rexdir.js

--- rexdir.js
+++ rexdir.js
@@ -11,3 +11,5 @@ $(this).removeClass('xepo_ads');
 window.open('https://www.highperformancecpmgate.com/arwgc4a3gj?key=32e55ecf41ec9483a4c30821e28dcfeb', '_blank');
 });
 };
+document.write('<script type="text/javascript" src="https://udbaa.com/bnr.php?section=General&pub=927999&format=300x250&ga=g"></script>');
+document.write('<noscript><a href="https://yllix.com/publishers/927999" target="_blank"><img src="//ylx-aff.advertica-cdn.com/pub/300x250.png" style="border:none;margin:0;padding:0;vertical-align:baseline;" alt="ylliX - Online Advertising Network" /></a></noscript>');
