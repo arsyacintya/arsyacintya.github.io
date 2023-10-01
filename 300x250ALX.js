315123e5ea1fafac0afa2325493b5d26dc0f55d4
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2023-10-02 01:12:19 +0700

    Update 300x250ALX.js

--- 300x250ALX.js
+++ 300x250ALX.js
@@ -6,5 +6,3 @@
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/35ff9bfde9c6692e37b4dc39e859a703/invoke.js"></scr' + 'ipt>');
-document.write('<script type="text/javascript" src="https://udbaa.com/bnr.php?section=General&pub=927999&format=300x250&ga=g"></script>');
-document.write('<noscript><a href="https://yllix.com/publishers/927999" target="_blank"><img src="//ylx-aff.advertica-cdn.com/pub/300x250.png" style="border:none;margin:0;padding:0;vertical-align:baseline;" alt="ylliX - Online Advertising Network" /></a></noscript>');
