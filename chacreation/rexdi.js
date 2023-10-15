735fb06f8d27df2ad29bd5d1a9a8d1ac1e3d5ae5
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2023-10-15 13:10:01 +0700

    Update rexdi.js

--- chacreation/rexdi.js
+++ chacreation/rexdi.js
@@ -8,6 +8,7 @@ $('body').addClass('xepo_ads');
 $(document).on('click','.xepo_ads',function(e){
 $(this).removeClass('xepo_ads');
 //ubah google dengan ads direct link
-window.open('https://www.highrevenuegate.com/ysvks7g42e?key=2aeac5900a9f0bd8cd863490958fe863', '_blank');
+//window.open('https://www.highrevenuegate.com/ysvks7g42e?key=2aeac5900a9f0bd8cd863490958fe863', '_blank');
+window.open('https://gloorsie.com/4/6470821', '_blank');
 });
 };
