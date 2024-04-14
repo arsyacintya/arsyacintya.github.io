3469110cedb530815384060db54fe66a1f8eca60
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2024-04-15 03:20:32 +0700

    Update tunul.js

--- tunul.js
+++ tunul.js
@@ -1,10 +1,10 @@
 document.writeln("<!--script type=\"text/javascript\">");
 document.writeln("	atOptions = {");
-document.writeln("		\'key\' : \'8da6deb0bfbda4ed848b5f82176a8e9b\',");
+document.writeln("		\'key\' : \'ea1f17d0b969f24752ab804615e84b6b\',");
 document.writeln("		\'format\' : \'iframe\',");
 document.writeln("		\'height\' : 90,");
 document.writeln("		\'width\' : 728,");
 document.writeln("		\'params\' : {}");
 document.writeln("	};");
-document.writeln("	document.write(\'<scr\' + \'ipt type=\"text/javascript\" src=\"http\' + (location.protocol === \'https:\' ? \'s\' : \'\') + \'://www.profitabledisplaynetwork.com/8da6deb0bfbda4ed848b5f82176a8e9b/invoke.js\"></scr\' + \'ipt>\');");
+document.writeln("	document.write(\'<scr\' + \'ipt type=\"text/javascript\" src=\"http\' + (location.protocol === \'https:\' ? \'s\' : \'\') + \'://www.topcreativeformat.com/ea1f17d0b969f24752ab804615e84b6b/invoke.js\"></scr\' + \'ipt>\');");
 document.writeln("</script-->");
