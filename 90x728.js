f6f70537bdff64089908eefde80c370c8b5fda2d
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2024-07-05 01:43:09 +0700

    Update 90x728.js

--- 90x728.js
+++ 90x728.js
@@ -5,4 +5,4 @@
 		'width' : 728,
 		'params' : {}
 	};
-document.write('<script type="text/javascript" src="//www.topcreativeformat.com/ea1f17d0b969f24752ab804615e84b6b/invoke.js"></script>');
+document.write('<script type="text/javascript" src="//menacehabit.com/ea1f17d0b969f24752ab804615e84b6b/invoke.js"></script>');
