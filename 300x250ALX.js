c4508a4b28ffdd6dee8af04fcc46e9438d822d77
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2024-09-22 03:01:21 +0700

    Update 300x250ALX.js

--- 300x250ALX.js
+++ 300x250ALX.js
@@ -1,8 +1,8 @@
 	atOptions = {
-		'key' : '2f2cd4c04b0b336023f7481c876aa8ea',
+		'key' : '35ff9bfde9c6692e37b4dc39e859a703',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-document.write('<script type="text/javascript" src="//menacehabit.com/2f2cd4c04b0b336023f7481c876aa8ea/invoke.js"></script>');
+document.write('<script type="text/javascript" src="//www.topcreativeformat.com/35ff9bfde9c6692e37b4dc39e859a703/invoke.js"></script>');
