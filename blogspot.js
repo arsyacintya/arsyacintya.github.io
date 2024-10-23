52c98ec881710bfbabb7dfd183a976a470aa2c74
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2024-10-23 14:19:18 +0700

    Update blogspot.js

--- blogspot.js
+++ blogspot.js
@@ -1,1 +1,9 @@
 
+atOptions = {
+		'key' : '35ff9bfde9c6692e37b4dc39e859a703',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+document.write('<script type="text/javascript" src="//www.topcreativeformat.com/35ff9bfde9c6692e37b4dc39e859a703/invoke.js"></script>');
