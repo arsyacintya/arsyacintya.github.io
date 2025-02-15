56a45b9a1ed8e163baa31ca4d658e68dd1a108c2
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2025-02-16 00:34:59 +0700

    Create native.js

---
+++ doni/native.js
@@ -0,0 +1,2 @@
+document.write('<script async="async" data-cfasync="false" src="//pl25879086.effectiveratecpm.com/b11ee41fb5ff303c3713520935cefec6/invoke.js"></script>
+<div id="container-b11ee41fb5ff303c3713520935cefec6"></div>');
