87a8de2504556908a2fd520abb095825cef1172c
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2025-02-16 00:15:53 +0700

    Create 300x250.js

---
+++ doni/300x250.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '67aee748ea9ed1cd32e1b23251b206c2',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+document.write('<script type="text/javascript" src="//www.highperformanceformat.com/67aee748ea9ed1cd32e1b23251b206c2/invoke.js"></script>');
