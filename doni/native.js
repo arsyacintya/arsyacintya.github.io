d10c84f7de147d88917f4a829b261d79aef1ce52
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2025-02-16 00:38:52 +0700

    Update native.js

--- doni/native.js
+++ doni/native.js
@@ -1,2 +1,1 @@
-document.write('<script async="async" data-cfasync="false" src="//pl25879086.effectiveratecpm.com/b11ee41fb5ff303c3713520935cefec6/invoke.js"></script>
-<div id="container-b11ee41fb5ff303c3713520935cefec6"></div>');
+document.write("<script async=\"async\" data-cfasync=\"false\" src=\"\/\/pl25879086.effectiveratecpm.com\/b11ee41fb5ff303c3713520935cefec6\/invoke.js\"><\/script>\r\n<div id=\"container-b11ee41fb5ff303c3713520935cefec6\"><\/div>");
