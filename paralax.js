5b812eb79924d29dcd4df6eb39fe9696282a488e
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2025-02-23 04:33:33 +0700

    Update paralax.js

--- paralax.js
+++ paralax.js
@@ -1,10 +1,10 @@
 document.writeln("<!--script type=\"text/javascript\">");
 document.writeln("	atOptions = {");
-document.writeln("		\'key\' : \'28a5ce34bdb529d290595736475e2679\',");
+document.writeln("		\'key\' : \'5cc25e64f4ac5746eba90136f59fb246\',");
 document.writeln("		\'format\' : \'iframe\',");
 document.writeln("		\'height\' : 600,");
 document.writeln("		\'width\' : 160,");
 document.writeln("		\'params\' : {}");
 document.writeln("	};");
-document.writeln("	document.write(\'<scr\' + \'ipt type=\"text/javascript\" src=\"http\' + (location.protocol === \'https:\' ? \'s\' : \'\') + \'://www.topcreativeformat.com/28a5ce34bdb529d290595736475e2679/invoke.js\"></scr\' + \'ipt>\');");
+document.writeln("	document.write(\'<scr\' + \'ipt type=\"text/javascript\" src=\"http\' + (location.protocol === \'https:\' ? \'s\' : \'\') + \'://www.highperformanceformat.com/5cc25e64f4ac5746eba90136f59fb246/invoke.js\"></scr\' + \'ipt>\');");
 document.writeln("</script-->");
