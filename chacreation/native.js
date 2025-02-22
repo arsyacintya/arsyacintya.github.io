6dbd69361eeeeef46fe00b3e7d515fa525cab3d4
arsyacintya <120472381+arsyacintya@users.noreply.github.com>
2025-02-23 04:39:08 +0700

    Update native.js

--- chacreation/native.js
+++ chacreation/native.js
@@ -1,2 +1,2 @@
-document.write('<script async="async" data-cfasync="false" src="//pl23648107.profitablecpmrate.com/bc40a1b3bb7131032637f39a23d5fbe5/invoke.js"></script>');
-document.write('<div id="container-bc40a1b3bb7131032637f39a23d5fbe5"></div>');
+document.write('<script async="async" data-cfasync="false" src="//pl15808189.effectiveratecpm.com/1bd1934a2982baedfa995b272d386d33/invoke.js"></script>');
+document.write('<div id="container-1bd1934a2982baedfa995b272d386d33"></div>');
